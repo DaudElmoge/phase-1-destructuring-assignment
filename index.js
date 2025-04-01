f63ea4b4ccbd509d3d921d59e60014c468c9b4ac
Daud Abdiwahab Elmoge <daudwahab1320@gmail.com>
2025-04-01 16:29:09 +0300

    trial

--- index.js
+++ index.js
@@ -46,3 +46,79 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+// First doggie object
+// Destructuring Animal Sounds
+// Destructuring Animal Sounds
+// Destructuring Animal Sounds
+// Destructuring Animal Sounds// Destructuring Animal Sounds// Destructuring Animal Sounds
+const animalSounds = ['moo', 'baa', 'meow', 'woof', 'oink'];
+const [moo, baa, meow, woof, oink] = animalSounds;
+console.log(moo);    // "moo"
+console.log(baa);    // "baa"
+console.log(meow);   // "meow"
+console.log(woof);   // "woof"
+console.log(oink);   // "oink"
+
+// Destructuring Animal Names
+const animalNames = ['bessie', 'dolly', 'spot', 'whiskers'];
+const [bessie, dolly, spot, whiskers] = animalNames;
+console.log(bessie); // "bessie"
+console.log(dolly);  // "dolly"
+console.log(spot);   // "spot"
+console.log(whiskers); // "whiskers"
+
+// Destructuring Animal Colors
+const animalColors = ['blackAndWhite', 'black', 'white'];
+const [blackAndWhite, black, white] = animalColors;
+console.log(blackAndWhite); // "blackAndWhite"
+console.log(black);         // "black"
+console.log(white);         // "white"
+
+// Destructuring Rainbow Colors
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+console.log(red);    // "red"
+console.log(orange); // "orange"
+console.log(yellow); // "yellow"
+console.log(green);  // "green"
+console.log(blue);   // "blue"
+console.log(indigo); // "indigo"
+console.log(violet); // "violet"
+
+// Destructuring Rainbow Color Initials
+const [v, o, y, g, b, i] = ['violet', 'orange', 'yellow', 'green', 'blue', 'indigo'];
+console.log(v); // "violet"
+console.log(o); // "orange"
+console.log(y); // "yellow"
+console.log(g); // "green"
+console.log(b); // "blue"
+console.log(i); // "indigo"
+
+// Destructuring Indigo
+const [indg] = rainbowColors.slice(5, 6); // Only taking 'indigo'
+console.log(indg); // "indigo"
+
+// Destructuring from Object for Muppets
+const anotherMuppet = {
+  muppetName: 'Kermit',
+  job: 'Frog',
+  partner: 'Miss Piggy',
+  details: {
+    nestedJob: 'Show Host',
+    color: 'Green'
+  }
+};
+
+const { muppetName, job, partner, details: { nestedJob, color } } = muppet;
+console.log(muppetName); // "Kermit"
+console.log(job);        // "Frog"
+console.log(partner);    // "Miss Piggy"
+console.log(nestedJob);  // "Show Host"
+console.log(color);      // "Green"
+
+// Destructuring Songs (2 and 4)
+const songs = ['song1', 'Moving Right Along', 'song3', 'I Hope That Something Better Comes Along'];
+const [, song2, , song4] = songs;
+console.log(song2); // "Moving Right Along"
+console.log(song4); // "I Hope That Something Better Comes Along"
